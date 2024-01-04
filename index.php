b0a25bf3aa3e8978fbd456fc2e7a5967c5bfa67c
Maxime <maxime.lecieuxlmf@gmail.com>
2024-01-04 11:22:39 +0100

    Intégration de la première section de présentation + ajout de l'image background et du fichier style.css dans les assets

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Garage V.Parrot</title>
 </head>
 <body>
-    <div class="container">
+    <div class="container-fluid p-0">
         <header>
-            <nav class="navbar navbar-expand-md fixed-top border-bottom">
+            <nav class="navbar navbar-expand-md border-bottom">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">
                         <img src="assets/images/logo.svg" width="200" alt="Logo">
@@ -35,9 +36,14 @@
                 </div>
             </nav>
         </header>
-
-
-
+        <section class="position-relative first-section">
+            <div class="px-4 py-5 text-center position-absolute top-50 start-50 translate-middle text-white">
+                <h1 class="display-5 fw-bold">Garage V.Parrot</h1>
+                <div class="col-lg-6 mx-auto">
+                    <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                </div>
+            </div>
+        </section>
         <footer class="py-3 my-4">
             <ul class="nav justify-content-center border-bottom pb-3 mb-3">
                 <li class="nav-item m-2"><a href="#" class="nav-link">Accueil</a></li>
@@ -46,7 +52,7 @@
                 <li class="nav-item m-2"><a href="#" class="nav-link">Véhicules</a></li>
                 <li class="nav-item m-2"><a href="#" class="nav-link">Connexion</a></li>
             </ul>
-            <p class="text-center text-body-secondary">© 2023 Company, Inc</p>
+            <p class="text-center text-body-secondary">© 2024 Garage V.Parrot, Inc</p>
         </footer>
     </div>
 
