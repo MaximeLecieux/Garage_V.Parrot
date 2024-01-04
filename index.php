0a4e8aae92407ee1c20eabed4d419cb76414b0ab
Maxime <maxime.lecieuxlmf@gmail.com>
2024-01-04 17:57:19 +0100

    Début d'intégration de l'affichage des horaires d'ouvertures + Coordonnées

--- index.php
+++ index.php
@@ -221,10 +221,37 @@
 
 
         <footer class="py-3 my-4">
-            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <div class="row text-center">
+                <div class="col-lg-6">
+                    <h2 class="m-5">Horaires d'ouvertures</h2>
+                    <div>
+                        <ul>
+                            <li class="mb-2">Lundi : 8:00-12:00 , 14:00-18:00</li>
+                            <li class="mb-2">Mardi : 8:00-12:00 , 14:00-18:00</li>
+                            <li class="mb-2">Mercredi : 8:00-12:00 , 14:00-18:00</li>
+                            <li class="mb-2">Jeudi : 8:00-12:00 , 14:00-18:00</li>
+                            <li class="mb-2">Vendredi : 8:00-12:00 , 14:00-18:00</li>
+                            <li class="mb-2">Samedi : 8:00-12:00 , Fermé</li>
+                            <li class="mb-2">Dimanche : Fermé , Fermé</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <h2 class="m-5">Coordonnées</h2>
+                    <div>
+                        <p>
+                            Garage V.PARROT <br>
+                            123 Rue de l'imaginaire, 31555 Toulouse
+                        </p>
+                        <p>
+                            06-XX-XX-XX-XX <br>
+                            contact@garagevparrot.fr
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <ul class="nav justify-content-center border-bottom py-3 my-3">
                 <li class="nav-item m-2"><a href="#">Accueil</a></li>
-                <li class="nav-item m-2"><a href="#">Services</a></li>
-                <li class="nav-item m-2"><a href="#">Horaires</a></li>
                 <li class="nav-item m-2"><a href="#">Véhicules</a></li>
                 <li class="nav-item m-2"><a href="#">Connexion</a></li>
             </ul>
