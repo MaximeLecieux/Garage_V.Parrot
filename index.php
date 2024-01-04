9732b25cb5e50a052382db044241d25a8b50e692
Maxime <maxime.lecieuxlmf@gmail.com>
2024-01-04 10:46:02 +0100

    Intégration du footer dans index.php

--- index.php
+++ index.php
@@ -8,32 +8,46 @@
 </head>
 <body>
     <div class="container">
-        <nav class="navbar navbar-expand-md fixed-top border-bottom">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <img src="assets/images/logo.svg" width="200" alt="Logo">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-                    <div class="offcanvas-body">
-                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <li class="nav-item m-2"><a href="#" class="nav-link">Accueil</a></li>
-                            <li class="nav-item m-2"><a href="#" class="nav-link">Services</a></li>
-                            <li class="nav-item m-2"><a href="#" class="nav-link">Horaires</a></li>
-                            <li class="nav-item m-2"><a href="#" class="nav-link">Véhicules</a></li>
-                            <li class="nav-item m-2"><a href="#" class="nav-link">Connexion</a></li>
-                        </ul>
+        <header>
+            <nav class="navbar navbar-expand-md fixed-top border-bottom">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="assets/images/logo.svg" width="200" alt="Logo">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li class="nav-item m-2"><a href="#" class="nav-link">Accueil</a></li>
+                                <li class="nav-item m-2"><a href="#" class="nav-link">Services</a></li>
+                                <li class="nav-item m-2"><a href="#" class="nav-link">Horaires</a></li>
+                                <li class="nav-item m-2"><a href="#" class="nav-link">Véhicules</a></li>
+                                <li class="nav-item m-2"><a href="#" class="nav-link">Connexion</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
 
+
+
+        <footer class="py-3 my-4">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item m-2"><a href="#" class="nav-link">Accueil</a></li>
+                <li class="nav-item m-2"><a href="#" class="nav-link">Services</a></li>
+                <li class="nav-item m-2"><a href="#" class="nav-link">Horaires</a></li>
+                <li class="nav-item m-2"><a href="#" class="nav-link">Véhicules</a></li>
+                <li class="nav-item m-2"><a href="#" class="nav-link">Connexion</a></li>
+            </ul>
+            <p class="text-center text-body-secondary">© 2023 Company, Inc</p>
+        </footer>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
