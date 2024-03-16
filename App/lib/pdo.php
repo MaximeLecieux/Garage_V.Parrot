ddad7ffad463aa7a9a1a142f19049242a9fcd8f0
Maxime Lecieux <51786396+MaximeLecieux@users.noreply.github.com>
2024-03-16 15:25:41 +0100

    Update pdo.php
    Add SET NAME utf-8

--- App/lib/pdo.php
+++ App/lib/pdo.php
@@ -1,7 +1,7 @@
 <?php 
 
 try {
-    $pdo = new PDO('mysql:dbname=garage;host=localhost', 'Administrateur', 'Password123!');
+    $pdo = new PDO('mysql:dbname=garage;host=localhost', 'Administrateur', 'Password123!', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 } catch (PDOException $PDOException){
     echo 'Impossible de contacter le serveur';
